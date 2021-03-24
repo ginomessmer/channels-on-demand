0481c3c13fb6a9e0786d5f4c315cef008f6f0db3
Gino Messmer <gino@messmerized.de>
2021-03-24 21:53:13 +0100

    Define create space command

--- src/DiscordVoiceChannelsOnDemand.Bot/Commands/SpaceCommands.cs
+++ src/DiscordVoiceChannelsOnDemand.Bot/Commands/SpaceCommands.cs
@@ -1,10 +1,41 @@
-﻿using Discord.Commands;
+﻿using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using DiscordVoiceChannelsOnDemand.Bot.Services;
 
 namespace DiscordVoiceChannelsOnDemand.Bot.Commands
 {
     [Group("space")]
     public class SpaceCommands : ModuleBase<SocketCommandContext>
     {
+        private readonly IServerService _serverService;
 
+        public SpaceCommands(IServerService serverService)
+        {
+            _serverService = serverService;
+        }
+
+        [Command("create")]
+        [Summary("Creates a new private text channel for you and all users you mention")]
+        public async Task CreateSpace(params IGuildUser[] users)
+        {
+            // Check whether space is set up/enabled on the server
+            var server = await _serverService.GetAsync(Context.Guild);
+            if (!server.IsSpacesEnabled)
+            {
+                // TODO: Reply
+                return;
+            }
+
+            // Create new text channel
+
+            // Set up permissions (add all users)
+
+            // Modify by moving to space category
+
+            // Insert in database
+
+            // Reply
+        }
     }
 }
--- src/DiscordVoiceChannelsOnDemand.Bot/Models/Server.cs
+++ src/DiscordVoiceChannelsOnDemand.Bot/Models/Server.cs
@@ -6,12 +6,35 @@ namespace DiscordVoiceChannelsOnDemand.Bot.Models
 {
     public class Server
     {
+        /// <summary>
+        /// The guild's ID. This is the primary identifier of the server.
+        /// </summary>
         [Key]
         public string GuildId { get; set; }
 
+        // Lobbies
+
+        /// <summary>
+        /// All active lobbies created on the server.
+        /// </summary>
         public ICollection<Lobby> Lobbies { get; set; } = new List<Lobby>();
 
+        // Spaces
+
+        /// <summary>
+        /// The category's ID for new spaces.
+        /// </summary>
+        public string SpaceCategoryId { get; set; }
+
+        /// <summary>
+        /// All active spaces created on the server.
+        /// </summary>
         public ICollection<Space> Spaces { get; set; } = new List<Space>();
+
+        /// <summary>
+        /// Determines whether Spaces is enabled on the server.
+        /// </summary>
+        public bool IsSpacesEnabled => !string.IsNullOrEmpty(SpaceCategoryId);
 
         public Lobby GetLobby(string id) => Lobbies.FirstOrDefault(x => x.TriggerVoiceChannelId == id);
     }
